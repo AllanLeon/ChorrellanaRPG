c6ad2e1e042bce78a5bf2a252e8a62ad0c578fd3
Allan Leon <aleon.107@outlook.com>
2016-04-18 20:25:00 -0400

    Fixed states bugs

--- game/main.js
+++ game/main.js
@@ -10,7 +10,7 @@ window.game = game = new Phaser.Game(900, 500, Phaser.AUTO, '');
 
 // Adds the necessary states to the game.
 game.state.add('Boot', Game.States.Boot);
-game.state.add('Play', Game.States.Sandbox);
+game.state.add('Play', Game.States.Play);
 
 // Starts the initial state.
 game.state.start('Boot');
