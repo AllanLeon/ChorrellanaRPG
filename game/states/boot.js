ffb3177fbf9796b91e21ae72801e7ba275420ca2
Sergio Oporto <Sergio.E.Oporto@ieee.org>
2016-04-26 22:57:06 -0400

    Added a weapon image

--- game/states/boot.js
+++ game/states/boot.js
@@ -7,6 +7,7 @@ States.Boot = {
 		this.game.load.image('dukeCollider', 'assets/images/characters/collider.png');
 		this.game.load.spritesheet('duke', 'assets/images/characters/duke.png', 23, 43);
 		this.game.load.image('map', 'assets/images/backgrounds/yourmommasmap.png');
+		this.game.load.image('stone', 'assets/images/weapons/stone.png');
 	},
 	// Starts the next state.
 	create: function(){
