3d6b0ccecbbd6180ae86e14692807b6b46a6e342
Mauricio Fernandez <maury844@gmail.com>
2016-05-01 13:21:47 -0400

    Testing my repo

--- game/states/boot.js
+++ game/states/boot.js
@@ -1,4 +1,4 @@
-var States = {};
+	var States = {};
 
 // Loads all the necesasary assets before starting the game.
 States.Boot = {
@@ -7,6 +7,12 @@ States.Boot = {
 		this.game.load.image('dukeCollider', 'assets/images/characters/collider.png');
 		this.game.load.spritesheet('duke', 'assets/images/characters/duke.png', 23, 43);
 		this.game.load.image('map', 'assets/images/backgrounds/yourmommasmap.png');
+		for (var i=0; i<2; i++){
+			var iStr = i.toString();
+			this.game.load.image('bullet'.concat(iStr),'assets/images/weapons/bullet/bullet'.concat(iStr.concat(".png")));
+			this.game.load.image('weapon'.concat(iStr),'assets/images/weapons/weapon'.concat(iStr.concat(".png")));	
+		}
+				
 	},
 	// Starts the next state.
 	create: function(){
