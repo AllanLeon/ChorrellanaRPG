03087320cad0fce4b03f71ed6993afd3924eccf4
Valeria Cespedes <valecespedes8@gmail.com>
2016-05-14 18:28:04 -0400

    added effect when click

--- game/models/effects.js
+++ game/models/effects.js
@@ -4,6 +4,23 @@ function effects(game){
 }
 
 var music;
+var manager = null;
+var emitter = null;
+var image = null;
+
+effects.prototype.clickBoom = function(pointer) {
+
+    var x = pointer.x;
+    var y = pointer.y;
+
+    //  This will apply the radiateFrom to only those particles emitted in this call
+    emitter.emit('basic', x - 48, y - 40, { zone: image, full: true, spacing: 8, setColor: true, radiateFrom: { x: x, y: y, velocity: 1 } });
+}
+
+effects.prototype.render = function() {
+
+    emitter.debug(432, 522);
+}
 
 effects.prototype.create = function() {
 
@@ -14,7 +31,24 @@ effects.prototype.create = function() {
 
     music.play();
 
-    //game.input.onDown.add(changeVolume, this);
+    manager = this.game.plugins.add(Phaser.ParticleStorm);
+
+    var data = {
+        lifespan: 3000
+    };
+
+    manager.addData('basic', data);
+
+    emitter = manager.createEmitter(Phaser.ParticleStorm.PIXEL);
+
+    emitter.renderer.pixelSize = 8;
+
+    emitter.addToWorld();
+
+    //  12 x 10 = 96 x 80 px
+    image = manager.createImageZone('heart');
+
+    game.input.onDown.add(clickBoom, this);
 }
 
 effects.prototype.changeVolume = function(pointer) {
@@ -35,4 +69,5 @@ effects.prototype.changeVolume = function(pointer) {
 
 effects.prototype.load = function(){
 	this.create();
+	game.forceSingleUpdate = true;
 }
