68b523361684f19f3b6f0cb51612e4294e3f606f
Valeria Cespedes <valecespedes8@gmail.com>
2016-05-19 00:06:12 -0400

    Ahora si

---
+++ game/models/inventory.js
@@ -0,0 +1,75 @@
+//Creating Inventory whit its properties
+function inventory(game)
+{
+	this.game = game;
+	this.coinImage = null;
+	this.keyImage = null;
+	this.numberCoins = 0;
+	this.numberKeys = 0;
+	this.numberCoinsText = null;
+	this.numberKeysText = null;
+	this.inventoryImage = null;
+	this.isInventoryDisplay = false;
+	this.inventoryDisplayKey = game.input.keyboard.addKey(Phaser.Keyboard.I);
+}
+//Variable containing all the initial position for inventory grid, coin and key sprites
+var	inventoryData = 
+{
+	topRightCorner : {topRightX: 620, topRightY:10},
+	keyCoinSize : 48,
+	inventorySlotSize: 47,
+	inventoryPosition : {inventoryPosX : 190 , inventoryPosY : 123.75} 
+};
+
+
+inventory.prototype.render = function() 
+{
+	//Render the coins and keys images with their respective texts
+	this.coinImage = this.game.add.sprite(inventoryData.topRightCorner.topRightX, inventoryData.topRightCorner.topRightY, 'coin');
+	this.coinImage.scale.setTo(1.5,1.5);
+	this.numberCoinsText = game.add.text(inventoryData.topRightCorner.topRightX + inventoryData.keyCoinSize , inventoryData.topRightCorner.topRightY +(0.15*inventoryData.keyCoinSize), this.numberCoins, { fontSize: '32px', fill: '#000' });
+	this.keyImage = this.game.add.sprite(inventoryData.topRightCorner.topRightX + (2*inventoryData.keyCoinSize), inventoryData.topRightCorner.topRightY, 'key');
+	this.keyImage.scale.setTo(1.5,1.5);
+	this.numberKeysText  = game.add.text(inventoryData.topRightCorner.topRightX + (3*inventoryData.keyCoinSize) , inventoryData.topRightCorner.topRightY + (0.15*inventoryData.keyCoinSize), this.numberKeys, { fontSize: '32px', fill: '#000' });
+	//Render the Inventory
+	this.inventoryImage = this.game.add.sprite(	inventoryData.inventoryPosition.inventoryPosX , inventoryData.inventoryPosition.inventoryPosY, 'inventory');
+	this.inventoryImage.scale.setTo(1.5,1.5);
+	this.inventoryImage.alpha = 0;
+};	
+
+//Function to load all assets in Inventory
+inventory.prototype.load = function()
+{
+	this.render();
+}
+
+// Checks if inventory is displayed, and shows it
+inventory.prototype.showInventory = function()
+{
+	this.inventoryDisplayKey.onDown.add(function(){
+		if(this.isInventoryDisplay)
+		{
+			this.inventoryImage.alpha = 0;
+			this.isInventoryDisplay = false;
+			game.pause = false;
+		}
+		else
+		{
+			this.inventoryImage.alpha = 1;
+			this.isInventoryDisplay = true;
+			game.pause = true;
+		}
+		//game.input.keyboard.removeKeyCapture(Phaser.Keyboard.I);
+	}, this);
+
+
+}
+
+
+//Function encharge of updating inventory items
+inventory.prototype.update = function() 
+{
+	this.showInventory();
+	this.numberKeysText.text = this.numberKeys;
+	this.numberCoinsText.text = this.numberCoins;
+};
---
+++ game/models/weapon.js
@@ -0,0 +1,86 @@
+// Declaration of weapon data.
+function Weapon(game){
+	this.game = game;
+	this.sprite = null; // Use for HUD only
+	this.bulletCooldownMaxTime = 10; // Max time between shots
+	this.bulletCooldownTime = this.bulletCooldownMaxTime; // Time passed after the last shot
+	this.bulletsOnScreen = 10; // Max number of bullets on screen
+	this.bulletIndex = 0; 
+	this.bulletArray = []; // Array to store our bullets
+	this.weaponType = 1; // The weapon in use
+
+	this.bomb = [];
+	this.bombsOnScreen = 1;
+	this.bombCooldown = 2;
+
+	var timer;
+
+}
+
+// Use the weapon's sprite for the HUD only
+// Initializes the weapon's sprites.
+Weapon.prototype.render = function(x, y){
+	this.sprite = this.game.add.sprite(x, y, 'weapon' + this.weaponType);
+};
+
+// Loads the weapon's sprites
+Weapon.prototype.load = function(){
+	this.loadBullet();
+	//this.loadBomb();
+	timer = game.time.create(false)
+	timer.start();
+}
+
+// Loads all of the bullets in our bullet array
+Weapon.prototype.loadBullet = function(){
+	for (var i=0; i < this.bulletsOnScreen; i++){
+		this.bulletArray[i] = new Bullet(this.game);
+		this.bulletArray[i].load(this.weaponType);
+	}
+
+	this.bomb = new Bomb(this.game);
+	this.bomb.load(this.weaponType);
+}
+
+/*Weapon.prototype.loadBomb = function(){
+	for (var i=0; i< this.bombsOnScreen; i++){
+		this.bomb = new Bomb(this.game);
+		this.bomb.load(this.weaponType);
+	}
+}
+*/
+
+Weapon.prototype.nextWeapon = function(){
+	
+	console.log(this.player.weapon.weaponType);
+	this.player.weapon.weaponType = (this.player.weapon.weaponType % 5) + 1;
+}
+
+Weapon.prototype.throwBomb = function(){
+	//if(this.)
+	this.bomb.throw(this.game.player.sprite.x, this.game.player.sprite.y, this.game.player.direction);
+
+
+	timer.add(500, this.bomb.stop, this);
+	timer.add(1000, this.bomb.dissapear, this);
+}
+
+// Starts the shot of a bullet. 
+Weapon.prototype.fireWeapon = function(){
+
+	if (this.bulletCooldownTime == this.bulletCooldownMaxTime) {
+		this.bulletArray[this.bulletIndex].fireBullet(this.game.player.sprite.x,this.game.player.sprite.y, this.game.player.direction,
+		 this.game.player.weapon.weaponType);		
+
+		this.bulletIndex = (this.bulletIndex + 1) % this.bulletsOnScreen;
+		this.bulletCooldownTime = 0;	
+	}
+	
+}
+
+// Controls the time between shots
+Weapon.prototype.cooldown = function(){
+	if (this.bulletCooldownTime < this.bulletCooldownMaxTime){
+		this.bulletCooldownTime++;
+	}
+}
