90658aa2bf45ea02cbb1343d9872b78c873eeae8
Vanessa Sanjinez <vane.sanji@gmail.com>
2016-05-31 00:24:21 -0400

    Added folder with my Stage

---
+++ game/stages/houseStage/houseStage.js
@@ -0,0 +1,43 @@
+/**
+ * Test state for demostration purposes.
+ */
+function HouseStage() {}
+
+/**
+ * Sets the state's basic configurations.
+ */
+HouseStage.prototype.init = function() {};
+
+/**
+ * Initializes the stage objects.
+ */
+HouseStage.prototype.create = function() {
+	// Sets the world bounds.
+	this.game.world.setBounds(0, 0, 1066, 800);
+	this.game.background = this.game.add.sprite(0, 0, 'houseStage');
+
+	// Creates and loads a Player object.
+	game.player = new Player(window.game);
+	game.player.load();
+
+
+	game.houseObstacle = new HouseObstacle(window.game);
+	game.houseObstacle.load();
+
+	this.game.camera.follow(this.game.player.colliderSprite);
+	
+	//game.obstacle = new Obstacle(window.game);
+	//game.obstacle.load();
+}
+
+/**
+ * Updates all the stages's objects.
+ */
+HouseStage.prototype.update = function(){
+	// Updates the player.
+	game.player.update(game.player);
+}
+
+
+// Adds this stage to the game's states.
+States.HouseStage = new HouseStage();
