58b951dfb8ec68a6535b5135d13ba4c01443ba88
Mauricio Fernandez <maury844@gmail.com>
2016-06-05 23:42:58 -0400

    Spearow not working, cant jump from clouds, gravity value needs to be extremely high for some reason

--- game/stages/mauricioStage.js
+++ game/stages/mauricioStage.js
@@ -1,12 +1,12 @@
 /**
  * Test state for demostration purposes.
  */
-function TestStage() {}
+function mauricioStage() {}
 
 /**
  * Sets the state's basic configurations.
  */
-TestStage.prototype.init = function() {
+mauricioStage.prototype.init = function() {
 	this.game.renderer.renderSession.roundPixels = true;
 	this.physics.startSystem(Phaser.Physics.ARCADE);
 };
@@ -14,11 +14,11 @@ TestStage.prototype.init = function() {
 /**
  * Initializes the stage objects.
  */
-TestStage.prototype.create = function() {
+mauricioStage.prototype.create = function() {
 
-
-	
 	this.game.background = this.game.add.sprite(0, 0, 'cloudsback');
+	this.game.spearow = this.game.add.sprite(300,290,'spearow');
+	this.game.spearow.enableBody = true;
 
 	// Sets the world bounds.
 	this.game.world.setBounds(0, 0, 800, 950);
@@ -27,22 +27,35 @@ TestStage.prototype.create = function() {
 	game.player = new Player(window.game);
 	game.player.load();
 	game.player.addGravity();
+	game.player.changeMovement();
 
 	this.game.camera.follow(this.game.player.colliderSprite);
 
 	//Creates and loads blocks
-	game.obstacle = new Obstacle(window.game, 'testStage');
+	game.obstacle = new Obstacle(window.game, 'mauricioStage');
 	game.obstacle.load();
 }
 
 /**
  * Updates all the stages's objects.
  */
-TestStage.prototype.update = function(){
+mauricioStage.prototype.update = function(){
 	// Updates the player.
 	game.player.update(game.player);
+
+	game.physics.arcade.overlap(this.game.player.colliderSprite, this.game.spearow, changeStage, null, this);
+
 }
+
+function changeStage(player, spearow)
+{
+	console.log("spearow");
+	this.game.input.reset();
+	this.game.sound.stopAll();
+	this.game.state.start('play', true);
+} 
 
 
 // Adds this stage to the game's states.
-States.TestStage = new TestStage();
+States.mauricioStage = new mauricioStage();
+
