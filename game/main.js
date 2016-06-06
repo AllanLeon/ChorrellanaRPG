4f507b38661caad18ab6ec770629b28a02d14060
Vanessa Sanjinez <vane.sanji@gmail.com>
2016-06-05 23:47:12 -0400

    Added Basement stage

--- game/main.js
+++ game/main.js
@@ -16,6 +16,7 @@ game.state.add('GameOver', Game.States.GameOver);
 // Adds the different stages of the game as states.
 game.state.add('TestStage', Game.States.TestStage);
 game.state.add('HouseStage', Game.States.HouseStage);
+game.state.add('BasementStage', Game.States.BasementStage);
 
 // Starts the initial state.
 game.state.start('Boot');
