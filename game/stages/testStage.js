ddfcc4f47ea809a9dcf8990538fff638b0137828
Vanessa Sanjinez <vane.sanji@gmail.com>
2016-06-09 12:44:54 -0400

    Modified by mistake, and had some troubles with the pull

--- game/stages/testStage.js
+++ game/stages/testStage.js
@@ -12,8 +12,14 @@ TestStage.prototype.init = function() {};
  * Initializes the stage objects.
  */
 TestStage.prototype.create = function() {
+	//Sets the world background
+	this.game.background = this.game.add.sprite(0, 0, 'map');
+
 	// Sets the world bounds.
+	
+
 	this.game.world.setBounds(0, 0, 800, 600);
+	
 
 	// Creates and loads a Player object.
 	game.player = new Player(window.game);
