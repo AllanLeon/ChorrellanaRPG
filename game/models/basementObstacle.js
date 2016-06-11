8e272faf2493237f425ff90cc80b9846808ff977
Vanessa Sanjinez <vane.sanji@gmail.com>
2016-06-11 02:45:59 -0400

    Finished all the obstacles in basement stage

--- game/models/basementObstacle.js
+++ game/models/basementObstacle.js
@@ -13,19 +13,134 @@ BasementObstacle.prototype.render = function(){
 	this.blocks.enableBody = true;
 
 	//visibility of blocks
-	//this.blocks.alpha = 0;
+	this.blocks.alpha = 0;
 	
-	//Group of blocks border of house
-	//UP border
-	this.block = this.blocks.create(100, 100, 'blockH');
+	//Left Up conner bookcase
+	this.block = this.blocks.create(0, 0, 'blockH');
 	this.block.body.immovable = true;
-	this.block.width = 100;
+	this.block.width = 190;
 	this.block.height = 100;
 
-	this.block = this.blocks.create(200, 200, 'blockH');
+	//Doors
+	this.block = this.blocks.create(142, 145, 'blockH');
 	this.block.body.immovable = true;
-	this.block.width = 100;
-	this.block.height = 100;
+	this.block.width = 38;
+	this.block.height = 72;
+
+	this.block = this.blocks.create(142, 265, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 38;
+	this.block.height = 80;
+
+	//Door entrance
+	this.block = this.blocks.create(0, 140, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 135;
+	this.block.height = 10;
+
+	this.block = this.blocks.create(0, 340, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 135;
+	this.block.height = 10;
+
+	//Group of bookcases
+	//Frist "floor"
+	//First one
+	this.block = this.blocks.create(260, 40, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Second!
+	this.block = this.blocks.create(666, 40, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Last
+	this.block = this.blocks.create(1075, 40, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 160;
+	this.block.height = 180;
+
+	//Second "floor"
+	//First one
+	this.block = this.blocks.create(260, 275, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Second!
+	this.block = this.blocks.create(666, 275, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Last
+	this.block = this.blocks.create(1075, 275, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 160;
+	this.block.height = 180;
+
+	//Third "floor"
+	//First one
+	this.block = this.blocks.create(260, 510, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Second!
+	this.block = this.blocks.create(666, 510, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Last
+	this.block = this.blocks.create(1075, 510, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 160;
+	this.block.height = 180;
+
+	//Fourth "floor"
+	//First one
+	this.block = this.blocks.create(260, 745, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Second!
+	this.block = this.blocks.create(666, 745, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 310;
+	this.block.height = 180;
+
+	//Last
+	this.block = this.blocks.create(1075, 745, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 160;
+	this.block.height = 180;
+
+	//On the left side (both of them)
+	
+	this.block = this.blocks.create(0, 510, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 185;
+	this.block.height = 180;
+
+	this.block = this.blocks.create(0, 745, 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = 200;
+	this.block.height = 90;
+
+
+
+	/*
+	this.block = this.blocks.create(, , 'blockH');
+	this.block.body.immovable = true;
+	this.block.width = ;
+	this.block.height = ;
+
+	*/
 
 
 
